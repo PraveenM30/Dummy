e5c777e0ccde0cbc31634ce64b580b547b46fed5
PraveenM30 <praveen.mohansundar9@gmail.com>
2024-12-06 18:02:14 +0530

    2nd commit

--- src/fol1/Test2.java
+++ src/fol1/Test2.java
@@ -1,6 +1,6 @@
 package fol1;
 
-public class Test1 {
+public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("Hello World !");
 		System.out.println("Hello World !");
