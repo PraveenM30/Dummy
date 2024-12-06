80d43b90049425ce186bc4b5e3e62e97ec1f5761
PraveenM30 <praveen.mohansundar9@gmail.com>
2024-12-06 18:12:00 +0530

    from dumm2 second change

--- src/fol1/Test22.java
+++ src/fol1/Test22.java
@@ -1,6 +1,6 @@
 package fol1;
 
-public class Test2 {
+public class Test22 {
 	public static void main(String[] args) {
 		System.out.println("Hello World !");
 		System.out.println("Hello World !");
